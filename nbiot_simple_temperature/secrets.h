165f748c36137355c38d826e7e5bb5fa91f0cd4a
Andreas Krause <a.krause@mm1-technology.de>
2018-04-06 13:40:39 +0200

    Added define NB_IOT_MQTTSN_PWD. define NB_IOT_COT_PWD is now legacy, to be removed. Added further explanations how to deal with access parameters.

--- nbiot_simple_temperature/secrets.h
+++ nbiot_simple_temperature/secrets.h
@@ -11,11 +11,21 @@
 /**
  * NB-IoT Credentials
  */
-
 #define NB_IOT_SERVER_IP      "172.25.102.151"
 #define NB_IOT_SERVER_PORT    1883
+
+/* TODO 
+ * Put your MQTT-SN access parameters here: 
+ * IMSI and MQTTSN password of your SIM card.
+ */
 #define NB_IOT_IMSI           "<IMSI>" 
-#define NB_IOT_COT_PWD        "<COT PWD>"
+#define NB_IOT_MQTTSN_PWD     "<YOUR_MQTTSN_PWD>"
+
+
+/* NOTE: Please keep this (legacy) define untouched.*/
+#define NB_IOT_COT_PWD        NB_IOT_MQTTSN_PWD
+
+
 
 /**
  * GSM Credentials
